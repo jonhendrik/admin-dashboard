002ee1a56f8dc11708e763d59e6ed9b2500884b6
Jon Hendrik Medina <JonHendrikD.Medina@pccw.com>
2021-04-15 01:14:01 +0800

    Update rest controllers

--- src/main/java/com/jhm/admdash/controller/AccountController.java
+++ src/main/java/com/jhm/admdash/controller/AccountController.java
@@ -3,14 +3,14 @@ package com.jhm.admdash.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.jhm.admdash.model.Account;
 import com.jhm.admdash.service.AccountService;
 
-@Controller
+@RestController
 @RequestMapping(value="/api")
 public class AccountController {
 
--- src/main/java/com/jhm/admdash/controller/CustomerController.java
+++ src/main/java/com/jhm/admdash/controller/CustomerController.java
@@ -1,15 +1,15 @@
 package com.jhm.admdash.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.jhm.admdash.model.Customer;
 import com.jhm.admdash.service.CustomerService;
 
-@Controller
+@RestController
 @RequestMapping(value="/api")
 public class CustomerController {
 
