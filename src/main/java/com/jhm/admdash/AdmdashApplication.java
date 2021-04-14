969df0500b10460e7313d2e491e10d5ccde92c01
jonhendrik <82501720+jonhendrik@users.noreply.github.com>
2021-04-14 20:55:15 +0800

    Initial commit

---
+++ src/main/java/com/jhm/admdash/AdmdashApplication.java
@@ -0,0 +1,13 @@
+package com.jhm.admdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmdashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmdashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhm/admdash/AdmdashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhm.admdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
